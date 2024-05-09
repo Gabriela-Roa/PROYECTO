e9c0b41b2d093aa21f7627d8e15fde5a837d1cfa
Gabriela Garcia <gabygr1718@gmail.com>
2024-05-09 16:43:12 -0500

    .

---
+++ proyectoClaro/usuarios.py
@@ -0,0 +1,12 @@
+def registrar_usuario(datos):
+    datos = dict(datos)
+    usuario={}
+    usuario["Nombre"]=input("Ingrese el nombre: ")
+    usuario["Direccion"]=input("Ingrese la direccion: ")
+    usuario["Contacto"]=input("Ingrese el contacto: ")
+    usuario["Correo electronico"]=input("Ingrese el correo electronico: ")
+    usuario["Categoría"]=input("Ingrese la categoría: ")
+    try:
+        usuario[]
+
+    
